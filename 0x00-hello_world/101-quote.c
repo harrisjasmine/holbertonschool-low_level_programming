6dffc9f5b7c8eee57946d9c397afd8ef552e4a63
Jasmine Harris <harrisjasmine@gmail.com>
2019-06-20 06:10:46 +0000

    Use write for quote.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@
 int main(void)
 {
 	int length_of_quote = sizeof(
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19"
 	);
 	write(
 		2,
