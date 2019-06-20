60ef5c7e6ef9f1ddea91753cca4217fd8382e07c
Jasmine Harris <harrisjasmine@gmail.com>
2019-06-20 22:42:15 +0000

    Add program that runs positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* main - Entry */
-
-/* Return: Always 0 (Success) */
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -11,6 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 {
 		printf("%d is positive\n", n);
