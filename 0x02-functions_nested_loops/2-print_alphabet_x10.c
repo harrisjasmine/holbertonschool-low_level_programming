955e1d6e09e5df843d6d22207ea69b648d5205a2
Jasmine Harris <harrisjasmine@gmail.com>
2019-06-25 20:33:30 +0000

    Add function that prints the alphabet 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,28 +1,8 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - function to print alphabet
- * Return: Always void.
- */
-
-void print_alphabet(void)
-{
-	int current_ascii_number = 97;
-	int last_ascii_number = 122;
-
-	while (current_ascii_number <= last_ascii_number)
-	{
-		_putchar((char) current_ascii_number);
-
-		current_ascii_number++;
-	}
-
-	_putchar('\n');
-}
-
-/**
  * print_alphabet_x10 - function that prints 10 times the alphabet
- *
+ * @void: void variable
  * Return: Always void
  */
 void print_alphabet_x10(void)
