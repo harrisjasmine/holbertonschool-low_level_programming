9218ae6b13acbff91e94632e2dbff8acf1117b1a
Jasmine Harris <harrisjasmine@gmail.com>
2019-06-26 04:43:31 +0000

    update holberton header format.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "holberton.h"
 
 /**
  * main - prints Holberton
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <unistd.h>
 
 /**
@@ -12,3 +14,19 @@ int _putchar(char c);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+#endif /* HOLBERTON_H */
