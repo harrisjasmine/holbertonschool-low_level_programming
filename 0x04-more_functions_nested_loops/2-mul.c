94bcba1f6c44bf2708d41a0db063ed313a3047d6
Jasmine Harris <harrisjasmine@gmail.com>
2019-06-27 17:58:29 +0000

    Add function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - function that multiplies two integers together
+ * @a: formal parameter for an integer
+ * @b: formal parameter for an integer
+ * Return: product of two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
