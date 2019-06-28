413513ade3e881761a55286c61887c5218aeb4da
Jasmine Harris <harrisjasmine@gmail.com>
2019-06-28 04:43:49 +0000

    Final 4.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,23 +2,26 @@
 
 /**
  * print_most_numbers - function that prints most numbers from 0 to 9
- * Return: 0 if successful
+ * Return: nothing
  */
 
 void print_most_numbers(void)
 {
 	int current_ascii_number = 48;
-	int last_ascii_number = 57;
+	int last_ascii_number = 58;
 
 	while (current_ascii_number < last_ascii_number)
 	{
-		current_ascii_number++;
-
 		if (current_ascii_number == 50 || current_ascii_number == 52)
 		{
+			current_ascii_number++;
 			continue;
 		}
-		_putchar(current_ascii_number);
+
+		_putchar((char) current_ascii_number);
+
+		current_ascii_number++;
 	}
+
 	_putchar('\n');
 }
