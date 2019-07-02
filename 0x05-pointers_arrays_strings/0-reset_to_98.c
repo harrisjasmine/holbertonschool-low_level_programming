687d9dd597281d9acc84d2bb9355f5c7533e99f6
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-02 03:07:06 +0000

    Updates to exercise 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,7 @@
 
 /**
  * reset_to_98 - function that takes a pointer to an int as a parameter
- * @*n: pointer *n as a parameter
- * Return: successful if returns 0
+ * @n: pointer *n as a parameter
  */
 
 void reset_to_98(int *n)
