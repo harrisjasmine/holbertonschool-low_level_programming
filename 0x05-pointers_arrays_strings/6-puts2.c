2171c4b487857dfc3fee8ddd72652b46afb8f4c1
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-02 22:04:57 +0000

    Add function that prints everyother character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - function that prints every other character
+ * @str: pointer string passed into function
+ */
+
+void puts2(char *str)
+{
+	int a;
+	int b;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		;
+	}
+	for (b = 0; b < (a-1); b++)
+	{
+		if (b % 2 == 0)
+		_putchar(str[b]);
+	}
+	_putchar('\n');
+}
