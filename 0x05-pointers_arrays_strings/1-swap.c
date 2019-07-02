b527cacf9d1dd2976d2c6ecdd01599d721b53ff1
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-02 03:36:25 +0000

    Update exercise 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,8 +2,8 @@
 
 /**
  * swap_int - function that swaps the value of two integers
- * @*a: integer pointer
- * @*b: integer pointer
+ * @a: integer pointer
+ * @b: integer pointer
  */
 
 void swap_int(int *a, int *b)
