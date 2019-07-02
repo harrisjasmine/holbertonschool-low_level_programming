721c7be3e843310beec8be89fa30442d372ac408
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-02 03:03:21 +0000

    Update for exercise 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int as a parameter
+ * @*n: pointer *n as a parameter
+ * Return: successful if returns 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <unistd.h>
+int _putchar(char c);
+
+void reset_to_98(int *n);
+
+#endif /* HOLBERTON_H */
