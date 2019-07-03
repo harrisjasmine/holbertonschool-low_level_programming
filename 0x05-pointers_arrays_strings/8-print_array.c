f8bd9ff013dd9299837308f6a6b711eeba2c1ad8
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-03 02:04:29 +0000

    Add updates to exercise 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,16 +3,17 @@
 
 /**
  * print_array - function that prints n elements of an array of integers
- * @a - pointer to an array of integers
- * @n - number passed in to print
+ * @a: pointer to an array of integers
+ * @n: number passed in to print
  */
 
 void print_array(int *a, int n)
 {
 	int b;
+
 	for (b = 0; b < n ; b++)
 	{
-		if (b == (n-1))
+		if (b == (n - 1))
 			printf("%d", (a[b]));
 		else
 		printf("%d, ", (a[b]));
