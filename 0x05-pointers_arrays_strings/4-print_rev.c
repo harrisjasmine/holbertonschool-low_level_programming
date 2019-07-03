5af38a5918b0574772b15e29ab4cae4a5827e6ea
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-03 02:49:57 +0000

    Print full string.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,15 @@
 void print_rev(char *s)
 {
 	int a;
-	int b;
 
 	for (a = 0; s[a] != '\0'; a++)
 	{
 		;
 	}
 
-	for (b = a; b > 0; b--)
+	for (; a >= 0; a--)
 	{
-		_putchar(s[b]);
+		_putchar(s[a]);
 	}
 
 	_putchar('\n');
