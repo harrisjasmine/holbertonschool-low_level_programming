8a0b7d1bc3d2ec55aa860c2cedc11efabb9e56c6
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-07 02:15:16 +0000

    Add updates to exercise 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * reverse_array - function that reverse the content of an array of integers
@@ -8,5 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
+	int i, temp;
 
+	for (i = 0; i != --n; i++)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+	}
 }
