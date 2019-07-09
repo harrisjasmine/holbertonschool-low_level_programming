62718434969eabc321cecce5e56bd3f9f1fa6285
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-09 21:16:14 +0000

    Add exercise 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string passed in
+ * @c: character to look fo
+ * Return: pointer to string where first occurence of character or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; *s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	return ('\0');
+}
