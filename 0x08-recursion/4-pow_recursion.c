58d38c36a2010dbc321820ae27ab188d9e45340a
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-10 23:51:52 +0000

    Add exercise 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - function that returns value of x raised to power of y
+ * @x: is the base
+ * @y: exponent
+ * Return: integer with the exponential product
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return x * _pow_recursion(x, y - 1);
+}
