eb53cc5eee503060e124081a7073f9a739eef9e5
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-10 04:34:21 +0000

    Add updates to exercise 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,8 +3,8 @@
 
 /**
  * print_diagsums - function that prints the sum of the two diagonal
- * @a:
- * @size:
+ * @a: string passed into function
+ * @size: number of row and column
  */
 
 void print_diagsums(int *a, int size)
