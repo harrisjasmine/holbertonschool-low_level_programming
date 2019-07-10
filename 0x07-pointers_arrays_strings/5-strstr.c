50dfb05280a50ae551b90ba703bd4bfc99ceaf7c
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-10 02:04:51 +0000

    Add updates to exercise 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,36 +3,39 @@
 
 /**
  * _strstr - function that locates a substring
- * @haystack:
- * @needle:
+ * @haystack: pointer to original character to string
+ * @needle: secondary string being compared to
  * Return: a pointer to the beginning of the located substring
  */
 
 char *_strstr(char *haystack, char *needle)
 {
 	char *start;
+	char *reset;
 
 	if (*needle == '\0')
 		return (haystack);
 
 	for (; *haystack != '\0'; haystack++)
 	{
-			for (start = needle; *start != '\0'; start++)
+		for (start = needle; *start != '\0'; start++)
+		{
+			reset = haystack;
+
+			if (*start == *reset)
 			{
-				if (*start == *haystack)
-				{
-					start++;
-					haystack++;
-				}
-				if (*start != *haystack)
-				{
-					break;
-				}
-				if (*start != '\0')
-				{
-					return (needle);
-				}
+				start++;
+				reset++;
 			}
+			if (*start != *reset)
+			{
+				break;
+			}
+			if (*start != '\0')
+			{
+				return (needle);
+			}
+		}
 	}
 	return (NULL);
 }
