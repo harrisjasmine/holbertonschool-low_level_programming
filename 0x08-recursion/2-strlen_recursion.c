73f55a4f2b629720269c984d687ebd70e4a863e7
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-10 22:55:16 +0000

    Add exercise 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _strlen_recursion - function that returns length of a string
  * @s: pointer to string being used
+ * Return: sum from the length of a string
  */
 
 int _strlen_recursion(char *s)
@@ -10,5 +11,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	else
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 }
