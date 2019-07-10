9526f8617837b436d4d94e094dbe5e17ec223068
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-10 04:30:11 +0000

    Add exercise 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
  * print_diagsums - function that prints the sum of the two diagonal
@@ -8,4 +9,16 @@
 
 void print_diagsums(int *a, int size)
 {
+	int square = size * size;
+	int i,j;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0, j = size - 1; i < square; i = i + size + 1, j = j + size - 1)
+	{
+		sum1 = sum1 + a[i];
+		sum2 = sum2 + a[j];
+	}
+		printf("%d, ", sum1);
+		printf("%d\n", sum2);
 }
