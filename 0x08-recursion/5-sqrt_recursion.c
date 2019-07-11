60ef5de8579a702025fc5e2ecfc6e032c08563a8
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-11 05:08:42 +0000

    Add updates to exercise 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,13 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - function that returns the natural square root of a number
- * @n: is the number passed into the sqrt
- * Return: the natural square of n
+ * recursion_helper - function that returns square root
+ * @squared_number: number passed in to find square root
+ * @current_root: number to increment for help
+ *
+ * Return: current_root for the square root
  */
-int recursion_helper(int squared_number, int current_root);
 
-int _sqrt_recursion(int n)
-{
-	return (recursion_helper(n, 0));
-}
-
-/**
- * recursion_helper - function that returns square room
- * @a: number passed in to find square root
- * @n: number to increment for help
- * Return: n for the square root
- */
 int recursion_helper(int squared_number, int current_root)
 {
 	if (current_root * current_root == squared_number)
@@ -26,4 +16,15 @@ int recursion_helper(int squared_number, int current_root)
 		return (-1);
 	else
 		return (recursion_helper(squared_number, current_root + 1));
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural square root of a number
+ * @n: is the number passed into the sqrt
+ * Return: the natural square of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (recursion_helper(n, 0));
 }
