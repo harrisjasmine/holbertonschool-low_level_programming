ca7fb5028719f72ca348c05b3a873ca2528c7983
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-12 06:13:31 +0000

    Add exercise 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -14,6 +14,14 @@ int _strlen_recursion(char *s)
 		return (1 + _strlen_recursion(s + 1));
 }
 
+/**
+ * check - check if letters equal each other
+ * @start: character pointer set the beginning
+ * @end: character pointer set the end
+ *
+ * Return: 1 if palindrom, 0 if not
+ */
+
 int check(char *start, char *end)
 {
 	if (*start == *end)
@@ -25,6 +33,11 @@ int check(char *start, char *end)
 		return (0);
 }
 
+/**
+ * is_palindrome - function to check if string is a palindrome
+ * @s: character pointer to check if palindrome
+ * Return: 1 if palindrome, 0 if not
+ */
 int is_palindrome(char *s)
 {
 	int x;
