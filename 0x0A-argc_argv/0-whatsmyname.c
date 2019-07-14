752b8f09dd5bee20da3740c7cfb411aec4fb3d05
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-14 23:50:41 +0000

    Add exercise 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function to print prog name
+ * @argc: number of arguments
+ * @argv: array of pointers to string
+ */
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
