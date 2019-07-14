8b1a01e83f4122be2691ff0869ad950614db0c86
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-14 23:54:53 +0000

    Add updates to exercise 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,11 @@
  * main - main function to print prog name
  * @argc: number of arguments
  * @argv: array of pointers to string
+ *
+ * Return: 0
  */
 
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
