70fbd663f7ed5e6f6e51004c725430f328c75c31
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-15 01:13:14 +0000

    Add exercise 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that multiplies two number
+ * @argc: number of arguments
+ * @argv: array of pointers to strings which are those arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int sum;
+		int first_number = atoi(argv[1]);
+		int second_number = atoi (argv[2]);
+
+		sum = first_number * second_number;
+		printf("%d\n", sum);
+	}
+	return (0);
+}
