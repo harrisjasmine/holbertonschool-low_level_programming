5e5210d01d8b749f738b767d7ae3515df1cd52f6
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-17 03:51:32 +0000

    Add exercise 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - function that frees a 2 dimensional grid
+ * @grid: pointer to previous function
+ * @height: pointer to height
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+			free(grid[i]);
+	}
+	free(grid);
+}
