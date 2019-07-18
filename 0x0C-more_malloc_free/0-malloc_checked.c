7dc3d21cb405a7d80c28e1b3992983b05393b709
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-18 21:04:11 +0000

    Add updates to exercise 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,15 +4,17 @@
 /**
  * malloc_checked - function that allocates memory using malloc
  * @b: pointer
+ *
+ * Return: void pointer
  */
 
 void *malloc_checked(unsigned int b)
 {
-	unsigned int *allo_mem;
+	void *allo_mem;
 
 	allo_mem = malloc(sizeof(unsigned int) * b);
 		if (allo_mem == NULL)
 			exit(98);
 
-	return(allo_mem);
+	return (allo_mem);
 }
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,4 +3,5 @@
 #include <unistd.h>
 void *malloc_checked(unsigned int b);
 
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif /* HOLBERTON_H */
