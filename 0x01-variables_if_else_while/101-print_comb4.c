cc68f1172f9e2674a02ca67438b33ab7b356b1d3
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-18 01:09:24 +0000

    Add exercise 12

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints all possible different combinations
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i, j, l;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (l = '0'; l <= '9'; l++)
+			{
+				if (i != j)
+					if (j != l)
+						if (i != l)
+							if (i < j)
+								if (j < l)
+								{
+									putchar(i);
+									putchar(j);
+									putchar(l);
+									if (i < '7')
+										putchar(',');
+										putchar(' ');
+								}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
