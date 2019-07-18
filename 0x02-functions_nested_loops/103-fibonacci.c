fec9e89e1c08ce0d7e1a579914c6fcd39ea0c449
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-18 05:54:10 +0000

    Add exercise 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
 /**
+ * main - program that prints sum of even valued fib num
  *
- *
+ * Return: 0 if successful
  */
 
 int main(void)
 {
-	long int i;
+	unsigned long num1 = 1;
+	unsigned long num2 = 2;
+	unsigned long sum = 0;
+	unsigned long even_sum = 2;
 
-	for (i = 1; i < 4,000,000; i++)
+	while (sum < 4000000)
 	{
-		printf("%li, ",
+		sum =  num1 + num2;
+			if (sum % 2 == 0)
+				even_sum = even_sum + sum;
+		num1 = num2;
+		num2 = sum;
 	}
+	printf("%lu", even_sum);
+	putchar('\n');
+	return (0);
 }
