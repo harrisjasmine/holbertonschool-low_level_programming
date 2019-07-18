972133bafe6d0710e07093954c13e3079fbd2fc8
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-18 06:03:25 +0000

    Add exercise 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - function to print first 50 fibonacci numbers
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long num1 = 1;
+	unsigned long num2 = 2;
+	unsigned long sum;
+
+	printf("%lu, %lu, ", num1, num2);
+	for (i = 0; i < 96; i++)
+	{
+		sum = num1 + num2;
+		printf("%lu", sum);
+		if (i != 95)
+			printf(", ");
+		num1 = num2;
+		num2 = sum;
+	}
+	putchar('\n');
+	return (0);
+}
