c4208a78f43e620d16df400f81c30ee9f8fd6646
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-19 20:11:26 +0000

    Add updates to exercise 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -15,11 +15,11 @@ int main(void)
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			sum = sum + 1;
+			sum = sum + i;
 		else if (i % 3 == 0)
-			sum = sum + 1;
+			sum = sum + i;
 		else if (i % 5 == 0)
-			sum = sum + 1;
+			sum = sum + i;
 		else
 			continue;
 	}
