f201cc4fae3dcc18785f0308eef98a3d86c34b8a
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-22 22:14:07 +0000

    Add exercise 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was compiled from
+ * Return: name of the file
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+
+	return (0);
+}
+
