416bfe828efaaf265ca88b8aae5323bd91ec834e
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-22 21:21:16 +0000

    Add exercise 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+
+/**
+ * print_dog - function that prints struct dog
+ * @d: pointer to struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (!d)
+		printf("(nil)");
+	if (d->name == NULL)
+		printf("Name: (nil)");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %.6f\n", d->age);
+	if (d->owner == NULL)
+		printf("Owner: (nil)");
+	else
+		printf("Owner: %s\n", d->owner);
+
+}
