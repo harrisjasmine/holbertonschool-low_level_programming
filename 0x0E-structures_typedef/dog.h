567ea6a04cf244ad40d1ef5059de792599d23c69
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-22 19:56:47 +0000

    Add exercise 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog metrics
+ * @name: character pointer containing name of dog
+ * @age: float containing age of dog
+ * @owner: character pointer containing owner name
+ *
+ * Description: struct that holds information on a struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
