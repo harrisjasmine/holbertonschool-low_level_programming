c91b23d3b0cd20c05fd3f15ac8317e529b6bec81
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-24 22:12:57 +0000

    Add updates to exercise 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,15 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
-	if (array && size > 0 && cmp)
+	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
 				return (i);
 		}
-			return (-1);
 	}
 
-	return (0);
+	return (-1);
 }
