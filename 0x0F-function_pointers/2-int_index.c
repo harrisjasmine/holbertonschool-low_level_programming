fb96e6b5128e18e8accf9205199fb56183392356
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-24 21:49:19 +0000

    Add exercise 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * int_index - function that searches for an integer
+ * @array: pointer to array passed in
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: index of match or -1 if no match
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -17,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
-					return (i);
+				return (i);
 		}
 			return (-1);
 	}
