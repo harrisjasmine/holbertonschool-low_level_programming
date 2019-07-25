db1361c260608bef97d73bcd66a63b5c15507ade
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-25 23:45:51 +0000

    Add exercise 9

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: number passed in to print
+ *
+ *
+ */
+
+void print_number(int n)
+{
+	int m;
+	int rem;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else
+		m = n;
+
+	if (m > 0)
+	{
+		rem = m % 10;
+		if (m / 10 > 0)
+		{
+			print_number(m / 10);
+		}
+		_putchar(rem + '0');
+	}
+}
