829a0f835a4dbea21ea7a0dc02f0a116647e31af
Jasmine Harris <harrisjasmine@gmail.com>
2019-07-27 19:35:21 +0000

    Updates to exercise 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -22,10 +22,12 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		value = va_arg(args, const unsigned int);
+		value = va_arg(args, int);
 
 		sum = sum + value;
 	}
+
+	va_end(args);
 
 	return (sum);
 }
